15f8e5a9cc0703c5d8aa407e754767315ec6f734
dankton96 <dankton96@hotmail.com>
2016-05-03 14:00:38 -0300

    Create Numeros_primos.cpp
    Software in C/C++ which find the 'n' first prime numbers which you define.

---
+++ Numeros_primos.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(void)
+{
+int num,aux,cont=0,aux2,i2;
+cout<<"Informe quais primeiros numeros primos deseja saber: ";
+cin>>aux;
+cout<<"Primeiros "<<aux<<" numeros primos:"<<endl<<endl;
+i2=1;
+aux2=0;
+if(aux==1){cout<<1<<endl;}
+else{
+		while(cont<aux)
+			{
+				aux2=0;
+				for(int i=1; i<=i2;i++)
+					{
+						if(i2%i==0){aux2++;}
+					}
+				if(aux2==2){cont++;cout<<i2<<endl;}
+				i2++;
+			}
+		}
+    }
